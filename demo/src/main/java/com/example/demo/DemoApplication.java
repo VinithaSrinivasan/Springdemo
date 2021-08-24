e22646df850cbb1a9660b93bbb38cf331f6f61eb
VinithaSrinivasan <vinithasrinivasan05@gmail.com>
2021-08-24 18:40:19 +0530

    Second commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//commit
+//commit 2
 @SpringBootApplication
 public class DemoApplication {
 
